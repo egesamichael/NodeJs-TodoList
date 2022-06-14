c5ac54df16db7fd7896f2872aef8a37067eb5b69
Egesa Michael <praisewebhosts@gmail.com>
2022-06-14 14:28:34 +0300

    Fixing heroku R10 error

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ res.redirect("/");
 });
 
 //the server is listening on port 3000 for connections
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
